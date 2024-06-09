b8d1231fd327366a40304e4a51730d2131c87597
Simonefleek <162296986+Simonefleek@users.noreply.github.com>
2024-06-08 23:24:17 -0400

    About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
-  
-export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <p>Design your About me page </p> 
+import React from "react";
+import "./About.css";
+import profilePic from "../assets/profile_pic_name.jpg"; // Replace with your actual file name
+
+const About = () => (
+  <div>
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={profilePic}
+          alt="Profile Pic"
+        />
       </div>
-    )
-  }
-}
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Simone Ware</div>
+        <div className="brief_description">
+          My name is Símone Ware, and I am a rising junior at the New York Institute of Technology, majoring in Computer Science with a focus on Cloud Engineering.
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default About;
